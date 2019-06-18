90fa0c7d025d2fe3db2646e76993f99f7c47319c
Jun Ohtani <johtani@gmail.com>
2019-06-18 14:23:07 +0900

    Add DNS operations

--- gcp/terraform.tf
+++ gcp/terraform.tf
@@ -1,8 +1,12 @@
+# Google provider settings
+# You MUST set GCP service account key file to GOOGLE_APPLICATION_CREDENTIALS env param.
+# Need gcloud command
 provider "google" {
   project     = "${var.project_id}"
   region      = "${var.region}"
 }
 
+# For get zone list
 data "google_compute_zones" "available" {}
 
 # Network settings
@@ -26,12 +30,15 @@ resource "google_compute_network" "default" {
   name = "test-network"
 }
 
+# DNS zone settings
+resource "google_dns_managed_zone" "demo_domain" {
+  name        = "demo-domain-zone"
+  dns_name    = "${var.domain}."
+}
 
 # create the database instance
-
 resource "google_compute_instance" "database" {
-  
-  name         = "backend-johtani-demo"
+  name         = "database-johtani-demo"
   machine_type = "${var.machine_type}"
   zone         = "${data.google_compute_zones.available.names[0]}"
   tags         = ["database"]
@@ -47,7 +54,13 @@ resource "google_compute_instance" "database" {
 }
 
 ## database DNS entries
-
+resource "google_dns_record_set" "database" {
+  name         = "backend.${google_dns_managed_zone.demo_domain.dns_name}"
+  managed_zone = "${google_dns_managed_zone.demo_domain.name}"
+  type         = "A"
+  ttl          = 60
+  rrdatas      = [ "${google_compute_instance.database.network_interface.0.access_config.0.nat_ip}" ]
+}
 
 # create the frontend instance
 
@@ -69,3 +82,18 @@ resource "google_compute_instance" "frontend" {
 }
 
 ## frontend DNS entries
+resource "google_dns_record_set" "frontend" {
+  name         = "frontend.${google_dns_managed_zone.demo_domain.dns_name}"
+  managed_zone = "${google_dns_managed_zone.demo_domain.name}"
+  type         = "A"
+  ttl          = 60
+  rrdatas      = [ "${google_compute_instance.frontend.network_interface.0.access_config.0.nat_ip}" ]
+}
+
+resource "google_dns_record_set" "www" {
+  name         = "www.${google_dns_managed_zone.demo_domain.dns_name}"
+  managed_zone = "${google_dns_managed_zone.demo_domain.name}"
+  type         = "A"
+  ttl          = 60
+  rrdatas      = [ "${google_compute_instance.frontend.network_interface.0.access_config.0.nat_ip}" ]
+}
