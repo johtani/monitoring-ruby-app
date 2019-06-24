793ecc7ef20e572494f260169e3da25cbc6bd572
Jun Ohtani <johtani@gmail.com>
2019-06-24 16:15:19 +0900

    Change image and about.html

--- ruby-app/app/models/question.rb
+++ ruby-app/app/models/question.rb
@@ -1,3 +1,5 @@
 class Question < ApplicationRecord
   has_many :votes, dependent: :destroy
+  validates :autor, length: { minimum: 2, message: '何か入力してください' }
+  validates :title, length: { minimum: 2, message: '何か入力してください' }
 end
