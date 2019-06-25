f6ae78fb3c2360c158ffbda7e7309517d8c3d58d
Jun Ohtani <johtani@gmail.com>
2019-06-25 09:46:03 +0900

    Typo

--- ruby-app/app/models/question.rb
+++ ruby-app/app/models/question.rb
@@ -1,5 +1,5 @@
 class Question < ApplicationRecord
   has_many :votes, dependent: :destroy
-  validates :autor, length: { minimum: 2, message: '何か入力してください' }
+  validates :author, length: { minimum: 2, message: '何か入力してください' }
   validates :title, length: { minimum: 2, message: '何か入力してください' }
 end
